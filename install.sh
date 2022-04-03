e193b431914c75719a9a94f7c6ff4658ae5a86c2
gnogeek <77178414+gnogeek@users.noreply.github.com>
2022-04-03 15:38:11 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+# Create new directory for nginx proxy manager
+mkdir -p npm
+cd npm
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+# Setup the repository 
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# Set up stable repository
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# Test the installation
+docker-compose --version
+
+# Create a docker-compose configuration file
+# Editing the file
+cat << EOF >> docker-compose.yml
+version: '3'
+services:
+  app:
+    image: 'jc21/nginx-proxy-manager:latest'
+    restart: unless-stopped
+    ports:
+      - '80:80'
+      - '81:81'
+      - '443:443'
+    volumes:
+      - ./data:/data
+      - ./letsencrypt:/etc/letsencrypt
+EOF
+
+##
+sudo docker-compose up -d
